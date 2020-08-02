c5f3668a8696f5e6bcf0b2745dbae6e2f6e70022
Ritwik <rittiksinha12@gmail.com>
2020-08-02 07:00:45 +0530

    files modified

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+	
+	"use strict";
+	// Your custom js code goes here.
+
+});
+$("#btnStats").click(function(){
+	$(".showreel").show();
+	$("#btnStats").hide()
+});
